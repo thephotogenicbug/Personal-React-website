988e429a172d0cf0c6b179a066d7c8772014c3d5
thephotogenicbug <navinkumarachari@gmail.com>
2021-06-26 23:56:36 +0530

    Wrap everthing into a section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,17 +5,20 @@ class Main extends Component{
 
     render(){
         return(
-            <div className="container">
+           <section className="bg-dark">
+                <div className="container">
                 <div className="row">
-                 <div className="col-md-6">
+                 <div className="col-md-6 " >
                    <h1 className="text-light" id="main-heading">Naveen Web Development Course Online</h1>
-                   <p className="text-white">Learn HTML CSS BOOTSTRAP JAVASCRIPT REACT NODEJS MYSQL ONLINE</p>  
+                   <p className="text-white">Learn HTML CSS BOOTSTRAP JAVASCRIPT REACT NODEJS MYSQL ONLINE</p>
+                   <button className="btn btn-primary">Sign up</button>  
                  </div>
                  <div className="col-md-6">
                      <img src={Hero} className="img-fluid" id="hero"/>
                  </div>
                 </div>
             </div>
+           </section>
         )
     }
 
