6b4eb5eaae2e3c3a4cea12f7f4c9cff5f685f7cc
thephotogenicbug <navinkumarachari@gmail.com>
2021-07-08 14:20:11 +0530

    Personal-react-website : Add React-helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import Home from './Components/home/home';
+import {Helmet} from 'react-helmet'
 function App() {
   return (
     <div >
+      <Helmet>
+        <meta charSet="UTF-8"/>
+        <title>thephotogenicbug.dev</title>
+        <meta name="description" content="thephotogenicbug portfolio"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="keywords" content=""/>
+      </Helmet>
      <Home/>
     </div>
   );
